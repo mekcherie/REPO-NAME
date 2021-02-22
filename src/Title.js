21466ff1a4c3cd1eb2794537cd80da98193d13de
Meklit Cherie <makywonde347@yahoo.com>
2021-02-21 16:14:46 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+    return (
+        <div className="Title">
+            <h1>REACTAPP</h1>
+                <div classNmae="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+        </div>
+    )
+}
+
+export default Title 
